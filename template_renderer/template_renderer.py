4af64cec00825b27f7f60abe820413f2a0d2fe64
Stefan Fenzke <stefan.fenzke@noris.de>
2023-09-07 00:36:51 +0200

    added type hint to template renderer

--- template_renderer/template_renderer.py
+++ template_renderer/template_renderer.py
@@ -3,7 +3,7 @@ from jinja2 import Template
 
 template_dir = os.environ.get('PULUMI_TEMPLATE_DIR') if os.environ.get('PULUMI_TEMPLATE_DIR') else 'templates'
 
-def render_template(filename: str, data = {}) -> str:
+def render_template(filename: str, data: dict = {}) -> str:
     with open(f'{template_dir}/{filename}.j2', 'r') as template_file:
         template = template_file.read()
         template = Template(template)
