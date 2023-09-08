3c4ad07056f0c452d64b5a2942031681464eeb41
Stefan Fenzke <stefan.fenzke@noris.de>
2023-09-08 17:27:36 +0200

    added a filter to var global export to not polute the global scope

--- config/__init__.py
+++ config/__init__.py
@@ -1,5 +1,13 @@
 import pulumi
 import importlib
 
+config_items = [
+    'providers',
+    'networks',
+    'base_images',
+    'volumes',
+    'vms'
+]
+
 config = importlib.import_module(f'.{pulumi.get_stack()}.vars', __name__)
-globals().update(vars(config))
+globals().update({key:vars(config).get(key) for key in config_items})
